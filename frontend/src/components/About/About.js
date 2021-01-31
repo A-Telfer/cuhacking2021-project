c7a361d858994b5c561cf882189a48b137f160bf
harshdeeply <harshdeepkairon@yahoo.com>
2021-01-31 07:58:14 -0800

    Update About.js

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -6,44 +6,26 @@ const About = () => {
       <div className="jumbotron">
         <div className="row align-items-center">
           <div className="col-md-12 col-lg-6">
-            <h2 style={{ borderRight: "1px solid gray" }}>
-              What is our project?
-            </h2>
+            <h2 style={{ borderRight: "1px solid gray" }}>Why this project?</h2>
           </div>
           <div className="col-lg-6">
             <p className="lead">
-              Blow the man down lateen sail haul wind yardarm scurvy long
-              clothes furl swab code of conduct heave to. Boatswain mutiny
-              square-rigged lee main sheet dead men tell no tales scuttle
-              marooned careen Arr. Trysail black jack chase smartly hang the jib
-              grapple long boat Spanish Main careen keel. Spike hogshead
-              brigantine scurvy fore bowsprit doubloon crow's nest to go on
-              account lanyard. Matey reef sails swing the lead Spanish Main
-              bilge water careen yardarm smartly Pieces of Eight Admiral of the
-              Black.
-            </p>
-          </div>
-        </div>
-        <br />
-        <br />
-        <hr style={{ width: "30rem" }} />
-        <br />
-        <br />
-        <div className="row align-items-center">
-          <div className="col-md-12 col-lg-6">
-            <h2 style={{ borderRight: "1px solid gray" }}>Who are we?</h2>
-          </div>
-          <div className="col-lg-6">
-            <p className="lead">
-              Blow the man down lateen sail haul wind yardarm scurvy long
-              clothes furl swab code of conduct heave to. Boatswain mutiny
-              square-rigged lee main sheet dead men tell no tales scuttle
-              marooned careen Arr. Trysail black jack chase smartly hang the jib
-              grapple long boat Spanish Main careen keel. Spike hogshead
-              brigantine scurvy fore bowsprit doubloon crow's nest to go on
-              account lanyard. Matey reef sails swing the lead Spanish Main
-              bilge water careen yardarm smartly Pieces of Eight Admiral of the
-              Black.
+              The divide between science and tech is rapidly becoming smaller
+              and smaller. However, one problem that has yet to be addressed
+              effectively is the challenge for labs moving from manual analysis
+              to model-model driven analysis. <br />
+              <br />
+              If you are a lab that requires a custom and adaptable solution to
+              set up a quick and easy multi-sensor collection system, then you
+              have come to the right place! <br />
+              <br />
+              Lastly, we also believe in Open Data. It helps with
+              reproducibility, providing additional evidence of results, and
+              reducing the need for additional animal experiments. <br />
+              <br />
+              We hope our high quality data will be a major contribution to the
+              future of Open Data in life sciences and beyond. It is clear that
+              a better system is needed and we aren't Just Another Openlab.
             </p>
           </div>
         </div>
