150b51610457a3d53fec9c202dcd2d6dc52676f2
Bilal <solidhydrogenblock@gmail.com>
2021-01-31 02:30:51 -0800

    added download endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 app = Flask(__name__)
 
+UPLOAD_DIRECTORY='/data/'
 
 @app.route('/')
 def hello():
@@ -13,6 +14,11 @@ def start_sensor():
 @app.route('/stop_sensor')
 def stop_sensor():
     return "This should stop 🛑 the sensor..."
+
+@app.route("/dl_file")
+def get_file():
+    """Download a file."""
+    return send_from_directory(UPLOAD_DIRECTORY, 'last.mp4', as_attachment=True)
 
 if __name__=='__main__':
     app.run()
