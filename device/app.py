a9f8141189e46c0fbad4af8baa8f62b4235f11bd
Andre Telfer <andretelfer@cmail.carleton.ca>
2021-01-31 07:25:47 -0500

    readded subprocess interupt

--- device/app.py
+++ device/app.py
@@ -24,11 +24,13 @@ def start_sensor():
 
     return "This should start the sensor..."
 
+
 @app.route('/stop_sensor')
 def stop_sensor():
     global ffmpeg_process 
 
     if ffmpeg_process and not ffmpeg_process.poll():
+        os.kill(ffmpeg_process.pid, 2)
         ffmpeg_process.wait()
 
     return "This should stop 🛑 the sensor..."
